b18f9d1e2592e28b7647d97a2916da3228272af0
Andy Cross <across@dextersystems.com>
2023-12-01 08:22:05 -0500

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,41 @@
 fun main() {
+    fun convertToDigit(str: String): String {
+        if (str.length == 1) return str
+
+        if (str == "one") return "1"
+        if (str == "two") return "2"
+        if (str == "three") return "3"
+        if (str == "four") return "4"
+        if (str == "five") return "5"
+        if (str == "six") return "6"
+        if (str == "seven") return "7"
+        if (str == "eight") return "8"
+        return "9"
+    }
+
+    fun convertToDigits(str: String): String {
+        return Regex("(?=(\\d|one|two|three|four|five|six|seven|eight|nine))")
+            .findAll(str)
+            .joinToString { convertToDigit(it.groups[1]?.value ?: "") }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.filter { x -> x.isDigit() } }
+            .sumOf { it.first().digitToInt() * 10 + it.last().digitToInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { convertToDigits(it) }
+            .sumOf { it.first().digitToInt() * 10 + it.last().digitToInt() }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1(input).println() // 54081
+    part2(input).println() // 54649
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,4 @@
+import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
@@ -6,8 +7,11 @@ import kotlin.io.path.readLines
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String) = File("data", "$name.txt")
+    .readLines()
 
+fun readText(name: String) = File("data", "$name.txt")
+    .readText()
 /**
  * Converts string to md5 hash.
  */
