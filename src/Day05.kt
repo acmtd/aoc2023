bb7c2a50bd4e655e679f1f44fbe2cfe05867aacb
Andy Cross <across@dextersystems.com>
2023-12-05 12:46:12 -0500

    clean things up a bit

--- src/Day05.kt
+++ src/Day05.kt
@@ -2,7 +2,7 @@ import java.io.File
 
 fun main() {
     fun createReverseMap(data: String): Map<LongRange, Long> {
-        val map = data.split("\n").drop(1).associate { line ->
+        return data.split("\n").drop(1).associate { line ->
             val digits = line.split(" ").filter { it.isNotEmpty() }.map { it.toLong() }
 
             val sourceRange = digits[0]..<digits[2] + digits[0]
@@ -10,12 +10,10 @@ fun main() {
 
             sourceRange to destStart
         }
-
-        return map
     }
 
     fun createMap(data: String): Map<LongRange, Long> {
-        val map = data.split("\n").drop(1).associate { line ->
+        return data.split("\n").drop(1).associate { line ->
             val digits = line.split(" ").filter { it.isNotEmpty() }.map { it.toLong() }
 
             val destRange = digits[1]..<digits[2] + digits[1]
@@ -23,52 +21,34 @@ fun main() {
 
             destRange to sourceStart
         }
-
-        return map
     }
 
     fun mapLookup(num: Long, map: Map<LongRange, Long>): Long {
         val range = map.keys.firstOrNull { num in it } ?: return num
-        val offset = num - range.first
-
-        return map[range]!! + offset
-    }
-
-    fun mapLookupRange(numList: List<Long>, map: Map<LongRange, Long>): List<Long> {
-        return numList.map { num -> mapLookup(num, map) }.distinct()
-    }
-
-    fun getSeedsPart1(seeds: String): List<LongRange> {
-        return seeds.substringAfter("seeds: ").split(" ")
-            .filter { it.isNotEmpty() }.map { it.toLong() }.map { it..it }
-    }
-
-    fun getSeedsPart2(seeds: String): List<LongRange> {
-        // these represent start numbers and numbers of seeds
-        return seeds.substringAfter("seeds: ").split(" ")
-            .filter { it.isNotEmpty() }.map { it.toLong() }.chunked(2).map { it[0]..<it[0] + it[1] }
+        return map[range]!! + num - range.first
     }
 
     fun part1(data: String): Long {
         val blocks = data.split("\n\n")
-        val seedRanges = getSeedsPart1(blocks[0])
 
         val listOfMaps = buildList<Map<LongRange, Long>> {
             blocks.drop(1).forEach { add(createMap(it)) }
         }
 
-        return seedRanges.minOf { r ->
-            listOfMaps.fold(r.toList()) { acc, next -> mapLookupRange(acc, next) }.min()
-        }
+        return blocks[0].removePrefix("seeds: ").split(" ")
+            .filter { it.isNotEmpty() }.map { it.toLong() }
+            .minOf { r -> listOfMaps.fold(r) { acc, next -> mapLookup(acc, next) } }
     }
 
     fun part2(data: String): Long {
         val blocks = data.split("\n\n")
-        val seedRanges = getSeedsPart2(blocks[0])
 
         val listOfMaps = buildList<Map<LongRange, Long>> {
             blocks.drop(1).forEach { add(createReverseMap(it)) }
         }.reversed()
+
+        val seedRanges = blocks[0].removePrefix("seeds: ").split(" ")
+            .filter { it.isNotEmpty() }.map { it.toLong() }.chunked(2).map { it[0]..<it[0] + it[1] }
 
         var loc: Long = 0
 
