48a9cecd7bb168aabbc9a9353222b6a8d30bd38f
Andy Cross <across@dextersystems.com>
2023-12-05 19:26:15 -0500

    little bit more cleanup

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,25 +1,18 @@
 import java.io.File
 
 fun main() {
+    fun String.digits(): List<Long> = this.split(" ").filter { it.isNotEmpty() }.map { it.toLong() }
+    fun String.mapData(): List<List<Long>> = this.split("\n").drop(1).map { it.digits()}
+
     fun createReverseMap(data: String): Map<LongRange, Long> {
-        return data.split("\n").drop(1).associate { line ->
-            val digits = line.split(" ").filter { it.isNotEmpty() }.map { it.toLong() }
-
-            val sourceRange = digits[0]..<digits[2] + digits[0]
-            val destStart = digits[1]
-
-            sourceRange to destStart
+        return data.mapData().associate { (source, dest, len) ->
+            (source..<len + source) to dest
         }
     }
 
     fun createMap(data: String): Map<LongRange, Long> {
-        return data.split("\n").drop(1).associate { line ->
-            val digits = line.split(" ").filter { it.isNotEmpty() }.map { it.toLong() }
-
-            val destRange = digits[1]..<digits[2] + digits[1]
-            val sourceStart = digits[0]
-
-            destRange to sourceStart
+        return data.mapData().associate { (source, dest, len) ->
+            (dest..<len + dest) to source
         }
     }
 
@@ -31,7 +24,7 @@ fun main() {
     fun part1(data: String): Long {
         val blocks = data.split("\n\n")
 
-        val listOfMaps = buildList<Map<LongRange, Long>> {
+        val listOfMaps = buildList {
             blocks.drop(1).forEach { add(createMap(it)) }
         }
 
@@ -43,7 +36,7 @@ fun main() {
     fun part2(data: String): Long {
         val blocks = data.split("\n\n")
 
-        val listOfMaps = buildList<Map<LongRange, Long>> {
+        val listOfMaps = buildList {
             blocks.drop(1).forEach { add(createReverseMap(it)) }
         }.reversed()
 
@@ -62,14 +55,10 @@ fun main() {
     }
 
     val testInput = File("data", "Day05_test.txt").readText()
-    println("tests")
     check(part1(testInput) == 35.toLong())
     check(part2(testInput) == 46.toLong())
 
     val input = File("data", "Day05.txt").readText()
-    println("part 1")
     part1(input).println() // 462648396
-
-    println("part 2")
     part2(input).println() // 2520479
 }
