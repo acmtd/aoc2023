798893ef48590e903ae7ef6351ac5efbb6ac0a18
Andy Cross <across@dextersystems.com>
2023-12-05 20:37:31 -0500

    one more round of cleanup

--- src/Day05.kt
+++ src/Day05.kt
@@ -2,63 +2,58 @@ import java.io.File
 
 fun main() {
     fun String.digits(): List<Long> = this.split(" ").filter { it.isNotEmpty() }.map { it.toLong() }
-    fun String.mapData(): List<List<Long>> = this.split("\n").drop(1).map { it.digits()}
+    fun String.mapData(): List<List<Long>> = this.split("\n").drop(1).map { it.digits() }
 
-    fun createReverseMap(data: String): Map<LongRange, Long> {
-        return data.mapData().associate { (source, dest, len) ->
-            (source..<len + source) to dest
-        }
-    }
-
-    fun createMap(data: String): Map<LongRange, Long> {
-        return data.mapData().associate { (source, dest, len) ->
-            (dest..<len + dest) to source
-        }
-    }
-
-    fun mapLookup(num: Long, map: Map<LongRange, Long>): Long {
+    fun lookup(num: Long, map: Map<LongRange, Long>): Long {
         val range = map.keys.firstOrNull { num in it } ?: return num
         return map[range]!! + num - range.first
     }
 
-    fun part1(data: String): Long {
-        val blocks = data.split("\n\n")
+    fun Long.traverseMap(mapList: List<Map<LongRange, Long>>) =
+            mapList.fold(this) { acc, next -> lookup(acc, next) }
 
-        val listOfMaps = buildList {
+    fun createReverseMap(data: String): Map<LongRange, Long> =
+            data.mapData().associate { (source, dest, len) ->
+                (source..<len + source) to dest
+            }
+
+    fun createMap(data: String): Map<LongRange, Long> =
+            data.mapData().associate { (source, dest, len) ->
+                (dest..<len + dest) to source
+            }
+
+    fun seedDigits(blocks: List<String>) = blocks[0].removePrefix("seeds: ").split(" ")
+            .filter { it.isNotEmpty() }.map { it.toLong() }
+
+    fun part1(blocks: List<String>): Long {
+        val mapList = buildList {
             blocks.drop(1).forEach { add(createMap(it)) }
         }
 
-        return blocks[0].removePrefix("seeds: ").split(" ")
-            .filter { it.isNotEmpty() }.map { it.toLong() }
-            .minOf { r -> listOfMaps.fold(r) { acc, next -> mapLookup(acc, next) } }
+        return seedDigits(blocks).minOf { it.traverseMap(mapList) }
     }
 
-    fun part2(data: String): Long {
-        val blocks = data.split("\n\n")
-
-        val listOfMaps = buildList {
+    fun part2(blocks: List<String>): Long {
+        val mapList = buildList {
             blocks.drop(1).forEach { add(createReverseMap(it)) }
         }.reversed()
 
-        val seedRanges = blocks[0].removePrefix("seeds: ").split(" ")
-            .filter { it.isNotEmpty() }.map { it.toLong() }.chunked(2).map { it[0]..<it[0] + it[1] }
+        val seedRanges = seedDigits(blocks).chunked(2)
+                .map { (start, len) -> start..<len + start }
 
-        var loc: Long = 0
-
-        while (true) {
-            loc++
-            val seed = listOfMaps.fold(loc) { acc, next -> mapLookup(acc, next) }
-            val result = seedRanges.firstOrNull { seed in it }
-
-            if (result != null) return loc
+        for (loc in 0..Long.MAX_VALUE) {
+            val seed = loc.traverseMap(mapList)
+            seedRanges.firstOrNull { seed in it } ?: continue
+            return loc
         }
+        return 0
     }
 
-    val testInput = File("data", "Day05_test.txt").readText()
+    val testInput = File("data", "Day05_test.txt").readText().split("\n\n")
     check(part1(testInput) == 35.toLong())
     check(part2(testInput) == 46.toLong())
 
-    val input = File("data", "Day05.txt").readText()
+    val input = File("data", "Day05.txt").readText().split("\n\n")
     part1(input).println() // 462648396
     part2(input).println() // 2520479
 }
