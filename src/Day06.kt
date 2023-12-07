ea69166ab67fa4ec5ef8a1de6472da26dbb8cd3a
Andy Cross <across@dextersystems.com>
2023-12-06 21:05:51 -0500

    polish and comment the alternate quadratic solution, make it work for all parts/tests

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,34 +1,45 @@
 import kotlin.math.sqrt
+import kotlin.math.floor
+import kotlin.math.ceil
 import kotlin.time.measureTime
+
 fun main() {
     fun permutations(time: Long): List<Long> = (1..<time).map { speed -> speed * (time - speed) }
 
     fun quadraticWins(time: Long, record: Long): Int {
-        // if (speed * (time - speed)) > record
-        // ST - S^2 > record
-        // -S^2 + ST - record > 0
-        // ax^2 + bx + c = 0
-        // a = -1, b = T, c = -record
-        // -b +/- sqrt(b^2 - 4ac)/2a
-
+        // apply quadratic equation solution:
+        // secondsToHold * (totalTime - secondsToHold) > record
+        // -seconds^2 + (seconds*totalTime) > record
+        // fits into quadratic, solve for seconds as x:
+        // ax^2 +bx + c > 0
+        // where a=-1, b = totalTime, c = -record
         val a = -1
         val b = time.toDouble()
         val c = -record.toDouble()
 
-        val root = sqrt(b*b - (4*a*c))
+        val root = sqrt(b * b - (4 * a * c))
 
-        return root.toInt()
+        val minVal = (-b + root) / (a * 2.0)
+        val maxVal = (-b - root) / (a * 2.0)
+
+        val minValRounded = floor(minVal + 1.0)
+        val maxValRounded = ceil(maxVal - 1.0)
+
+        return maxValRounded.toInt() - minValRounded.toInt() + 1
     }
 
     fun part1(input: List<String>): Int {
         val times = input.first().removePrefix("Time:")
-            .split(' ').filter { it.isNotBlank() }.map { it.toInt() }
+                .split(' ').filter { it.isNotBlank() }.map { it.toInt() }
 
         val records = input.last().removePrefix("Distance:")
-            .split(' ').filter { it.isNotBlank() }.map { it.toInt() }
+                .split(' ').filter { it.isNotBlank() }.map { it.toInt() }
 
-        return times.withIndex().map { (idx, time) -> permutations(time.toLong()).count { it > records[idx] } }
-            .reduce(Int::times)
+        return times.withIndex().map { (idx, time) -> quadraticWins(time.toLong(), records[idx].toLong()) }
+                .reduce(Int::times)
+
+//        return times.withIndex().map { (idx, time) -> permutations(time.toLong()).count { it > records[idx] } }
+//                .reduce(Int::times)
     }
 
     fun part2(input: List<String>): Int {
@@ -49,7 +60,7 @@ fun main() {
 
     measureTime {
         part1(input).println() // 4403592
-    }.also { it.println() } // 890us
+    }.also { it.println() } // 890us for bruteforce, 480us for quadratic (most time spent parsing)
 
     measureTime {
         part2(input).println() // 38017587
