af007f839df281743d68652e5b370ca626ddebaa
Andy Cross <across@dextersystems.com>
2023-12-07 14:07:26 -0500

    improve comments

--- src/Day07.kt
+++ src/Day07.kt
@@ -6,28 +6,30 @@ fun main() {
     fun toHand(data: String, values: Map<Char, Int>): Hand {
         val (cards, bid) = data.split(" ")
 
+        // Map the numeric equivalent of each card (14=A, 2=2, 1=Joker) to the number of cards of each type
         val cardMap = buildMap {
             cards.groupingBy { it }.eachCount().forEach { (key, value) -> put(values[key]!!, value) }
         }
 
-        val maxCardsOfOneType = cardMap.filter { it.key > 1 }.maxOfOrNull { it.value } ?: 0
+        val maxNonJokerCards = cardMap.filter { it.key > 1 }.maxOfOrNull { it.value } ?: 0
         val jokerCount = cardMap[1] ?: 0
 
-        // the base hand score runs from 50 (five of a kind) down to 10 (all different)
-        // with a bonus added for the in between hands (full house, two pair)
-        val bonus = cardMap.filter { it.key > 1 && it.value > 1 }.count().takeIf { it > 1 } ?: 0
-        val baseScore = (maxCardsOfOneType + jokerCount) * 10 + bonus
+        // The base hand score is the maximum number of cards of one type multiplied by ten
+        // with a two point bonus added for the hands with more than one pair (full house, two pair).
+        // Gives possible scores of 50 (5K), 40 (4K), 32 (FH), 30 (3K), 22 (2P), 20 (2K), 10 (HC)
+        val bonus = cardMap.filter { it.key > 1 && it.value in (2..3) }.count().takeIf { it == 2 } ?: 0
+        val baseScore = (maxNonJokerCards + jokerCount) * 10 + bonus
 
-        // the tie score consists of two digits for each card in the hand, concatenated
-        val tieScore = cards.map { (values[it]!!.toString().padStart(2, '0')) }
-            .joinToString("")
+        // The tie-breaker score consists of two digits for each card in the hand, concatenated
+        val tieScore = cards.map { (values[it]!!.toString().padStart(2, '0')) }.joinToString("")
 
-        // concatenate the base score and tie score and we get an overall score
-        // that can be trivially sorted to put the cards in order
+        // Concatenate the base score and tie score and we get an overall numeric
+        // score that can be trivially sorted to put the cards in order
         return Hand(cards, bid.toInt(), "$baseScore$tieScore".toLong())
     }
 
     fun calculate(input: List<String>, labels: String): Long {
+        // map each card letter to a number from 14 (Ace) down to 1 (Joker)
         val values = labels.withIndex().associate { (idx, lbl) -> lbl to 14 - idx }
 
         return input.map { toHand(it, values) }.sortedBy { it.score }
@@ -45,9 +47,9 @@ fun main() {
     val input = readInput("Day07")
     measureTime {
         part1(input).println() // 248105065
-    }.also { it.println() } // 16ms
+    }.also { it.println() } // about 20ms
 
     measureTime {
         part2(input).println() // 249515436
-    }.also { it.println() } // 5ms
+    }.also { it.println() } // about 8ms
 }
