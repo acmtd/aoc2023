bbe76cb9cbe0cb53b8dea0384c0c74d984b17067
Andy Cross <across@dextersystems.com>
2023-12-14 11:59:34 -0500

    day 14 part 2 solution

--- src/Day14.kt
+++ src/Day14.kt
@@ -2,35 +2,82 @@ import java.io.File
 import kotlin.time.measureTime
 
 fun main() {
-    fun rowsAndCols(pattern: String): Pair<List<String>, List<String>> {
-        val rows = pattern.split("\n")
-
-        val colCount = rows.first().length
-
-        val cols = buildList {
-            (0..<colCount).forEach { col ->
+    fun flipRowsCols(rows: List<String>): List<String> {
+        return buildList {
+            rows.indices.forEach { col ->
                 add(rows.indices.map { row -> rows[row][col] }.joinToString(""))
             }
         }
+    }
 
-        return Pair(rows, cols)
+    fun rows(pattern: String): List<String> = pattern.split("\n")
+
+    fun gridScore(input: String): Int {
+        val cols = flipRowsCols(rows(input))
+
+        return cols.sumOf { col -> col.indices.filter { col[it] == 'O' }.sumOf { cols.size - it } }
+    }
+
+    fun translate(input: String, direction: Char): String {
+        val rows = rows(input)
+
+        val items = if (direction == 'N' || direction == 'S') flipRowsCols(rows) else rows
+
+        val translated = items.map { item ->
+            buildList {
+                add(-1) // add the wall at the top
+                addAll(items.indices.filter { item[it] == '#' })
+                add(item.length) // add a wall at the bottom
+            }.zipWithNext { first, second ->
+                // our goal here is to output a substring showing how this section now looks
+                // it should still start and end with the "#" obviously, with the rest
+                // containing the smushed up rocks and the "." sign
+                val movingRocks = item.indices.filter { it in first..second }.count { item[it] == 'O' }
+
+                if (direction == 'N' || direction == 'W')
+                    "#" + "O".repeat(movingRocks) + ".".repeat(second - first - movingRocks - 1)
+                else
+                    "#" + ".".repeat(second - first - movingRocks - 1) + "O".repeat(movingRocks)
+            }
+                .joinToString("")
+                .removePrefix("#")
+        }
+
+        val result = if (direction == 'E' || direction == 'W') translated.joinToString("\n")
+        else flipRowsCols(translated).joinToString("\n")
+
+        return result
     }
 
     fun part1(input: String): Int {
-        val (_, cols) = rowsAndCols(input)
+        return gridScore(translate(input, 'N'))
+    }
 
-        return cols.sumOf { col ->
-            buildList {
-                add(-1) // add the wall at the top
-                addAll(col.indices.filter { col[it] == '#' })
-                add(col.length) // add a wall at the bottom
-            }.zipWithNext { first, second ->
-                // find out which rolling rocks can hit each rock
-                val movingRocks = col.indices.filter { it in first..second }.count { col[it] == 'O' }
+    fun part2(input: String): Int {
+        val directions = "NWSE"
 
-                val baseScore = col.length - first - 1
-                baseScore * movingRocks - (0..<movingRocks).sum()
-            }.sum()
+        val gridCache = hashMapOf<String, Int>()
+        val scores = mutableListOf<Int>()
+
+        var grid = input
+        var cycle = 0
+
+        while (true) {
+            // complete a cycle
+            cycle++
+            directions.forEach { dir -> grid = translate(grid, dir) }
+
+            scores.add(gridScore(grid))
+
+            val previousCycle = gridCache.getOrPut(grid) { cycle }
+
+            if (previousCycle < cycle) {
+                // everything prior to previousCycle is spent getting the grid into a periodic cycle
+                val cycleLength = cycle - previousCycle
+                val offset = (1000000000 - previousCycle).mod(cycleLength)
+
+                return scores[previousCycle + offset - 1]
+            }
         }
     }
 
@@ -41,5 +88,11 @@ fun main() {
 
     measureTime {
         part1(input).println() // 112046
-    }.also { it.println() } // 15ms
+    }.also { it.println() } // 20ms
+
+    check(part2(testInput) == 64)
+
+    measureTime {
+        part2(input).println() // 104619 is too low
+    }.also { it.println() } // 1.2s
 }
